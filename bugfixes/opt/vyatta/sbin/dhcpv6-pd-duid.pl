0472f388ab8cafd7b759fc84711eb32db068d901
Brian 'Redbeard' Harrington <redbeard@dead-city.org>
2018-03-29 22:41:23 -0700

    bugfix: Added debugging and fixes
    Don't run this commit as it has way too many debugging lines...
    
    That being said, this commit has been tested with the DUID
    '00:01:06:00:5a:bd:ce:74:52:54:00:76:ef:80'
    
    AMD64:
    
    ```
    [bharrington@leviathan bugfixes]$ DUID_FILE=/tmp/dhcp6c_duid2 opt/vyatta/sbin/dhcpv6-pd-duid.pl --action=set --duid='00:01:06:00:5a:bd:ce:74:52:54:00:76:ef:80'
    Using File: /tmp/dhcp6c_duid2
    Calling set_duid
    [bharrington@leviathan bugfixes]$ DUID_FILE=/tmp/dhcp6c_duid2 opt/vyatta/sbin/dhcpv6-pd-duid.pl --action=show
    Using File: /tmp/dhcp6c_duid2
    DUID 00:01:06:00:5a:bd:ce:74:52:54:00:76:ef:80
    ```
    
    ER-X (MIPSEL):
    ```
    ubnt@panic:~$ DUID_FILE=/tmp/dhcp6c_duid2 /tmp/dhcpv6-pd-duid.pl --action=set --duid='00:01:06:00:5a:bd:ce:74:52:54:00:76:ef:80'
    Using File: /tmp/dhcp6c_duid2
    Calling set_duid
    ubnt@panic:~$ DUID_FILE=/tmp/dhcp6c_duid2 /tmp/dhcpv6-pd-duid.pl --action=show
    Using File: /tmp/dhcp6c_duid2
    DUID 00:01:06:00:5a:bd:ce:74:52:54:00:76:ef:80
    ```
    
    Edgerouter Pro-8 (MIPS64):
    ```
    bharrington@ur1:~$ DUID_FILE=/tmp/dhcp6c_duid2 /tmp/dhcpv6-pd-duid.pl --action=set --duid='00:01:06:00:5a:bd:ce:74:52:54:00:76:ef:80'
    Using File: /tmp/dhcp6c_duid2
    Calling set_duid
    bharrington@ur1:~$ DUID_FILE=/tmp/dhcp6c_duid2 /tmp/dhcpv6-pd-duid.pl --action=show
    Using File: /tmp/dhcp6c_duid2
    DUID 00:01:06:00:5a:bd:ce:74:52:54:00:76:ef:80
    
    ```
    
    At this point, I'll begin cleaning up the debugging statements.

--- bugfixes/opt/vyatta/sbin/dhcpv6-pd-duid.pl
+++ bugfixes/opt/vyatta/sbin/dhcpv6-pd-duid.pl
@@ -28,26 +28,38 @@ sub show_duid {
  
     open(my $FH, '<', $dhcp6c_duid) || die "Error opening [$dhcp6c_duid]: $!\n";
     binmode($FH);
-    my ($buf, $val, $len);  
+    my ($buf, $val, $len, $pack_template);  
     read($FH, $buf, 2);
     my $endian = $Config{byteorder};
     if ($endian == 1234 or $endian == 12345678) {
-        $len = unpack("v", $buf);
+        $pack_template = 'v'; 
     } else {
-        $len = unpack("n", $buf);
+        $pack_template = 'n'; 
     }
+    $len = unpack($pack_template, $buf);
+
     for (my $i = 0; $i < $len; $i++) {
         read($FH, $buf, 1);         
         $val .= unpack("H", $buf);
+	#print("debug 1: ", $val, "\n");
         $val .= unpack("h", $buf);
+	#print("debug 2: ", $val, "\n");
         $val .= ':' if $i + 1 < $len;
+	#print("debug 3: ", $val, "\n");
     }                                 
     close($FH);        
     print "DUID $val\n";    
 }
 
 sub set_duid {
+    print("Calling set_duid\n");
     my ($duid) = @_;
+    my ($pack_template);
+    if ($Config{byteorder} == 1234 or $Config{byteorder} == 12345678) {
+        $pack_template = 'v'; 
+    } else {
+        $pack_template = 'n'; 
+    }
 
     open(my $FH, '>', $dhcp6c_duid) || die "Error opening [$dhcp6c_duid]: $!\n";
     binmode($FH);
@@ -57,7 +69,7 @@ sub set_duid {
     $duid =~ s/://g;
     # print $duid;
     my $len = length($duid) / 2;
-    my $buf = pack("n", $len);
+    my $buf = pack($pack_template, $len);
     print $FH $buf || die "duid length write failed: $!\n";
 
     my $bin_duid = pack("H*", $duid);
@@ -66,14 +78,24 @@ sub set_duid {
 }
 
 sub set_duid_bin {
+    print("Calling set_duid_bin\n");
     my ($duid) = @_;
+    my ($pack_template);
+    if ($Config{byteorder} == 1234 or $Config{byteorder} == 12345678) {
+        $pack_template = 'v'; 
+    } else {
+        $pack_template = 'n'; 
+    }
+
 
     open(my $FH, '>', $dhcp6c_duid) || die "Error opening [$dhcp6c_duid]: $!\n";
     binmode($FH);
     
     $duid =~ s/://g;
     my $len = length($duid);
-    my $buf = pack("n", $len);
+    print("Length: ", $len, "\n");
+    # This is crazy because the generated length is 13
+    my $buf = pack($pack_template, $len);
     print $FH $buf || die "write failed\n";
 
     print $FH $duid;
@@ -94,6 +116,11 @@ sub get_mac {
 }
 
 sub gen_duid {
+    # Hypothetically it seems this function is attempting to implement RFC3315
+    # section 9.2 (DUID Based on Link-layer Address Plus Time [DUID-LLT])...
+    # That being said, the RFC states to use 32 bits for time while that for
+    # loop is only using 24.  In addition, the hardware type states to use 16 bits
+    #
     my ($intf) = @_;
 
     $intf = 'eth0' if !defined $intf;
@@ -103,18 +130,29 @@ sub gen_duid {
     }
     my $duid;
     my $duid_type = 1;
+    # Why is this being defined but not used?
     my $duid_intf = 6;  # ethernet
     my $duid_time = time();
     my @duid_mac = split(/:/, $mac);
+    print("DUID Length: ", length($duid), "\n");
+    # Inset a null character
     $duid  = chr(0);
+    # Inset ASCII Start of Header (character code 1)
     $duid .= chr($duid_type);
-    $duid .= chr($duid_type);
-    for (my $i=24; $i >= 0; $i -= 8) {
-		$duid .= chr(($duid_time >> $i) & 0xff);
+    # Insert a second ASCII SOH, because why not?
+    $duid .= chr($duid_intf);
+    
+    for (my $i=32; $i >= 0; $i -= 8) {
+	$duid .= chr(($duid_time >> $i) & 0xff);
+	print("DUID: ", $duid, "\n");
 	}
     foreach my $b (@duid_mac) {
         $duid .= chr(hex($b));
+	    print("DUID Length: ", length($duid), "\n");
     }
+
+    print("DUID Length: ", length($duid), "\n");
+    print(length($duid));
     set_duid_bin($duid);
 }
 
